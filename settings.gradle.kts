b0e7bc3ab0831de047f45a61493bbbaf302e3d08
Yaroslav Kishchenko <yaroslavkishchenko@gmail.com>
2022-02-09 00:06:14 +0300

    issue #2 Created Kotlin project structure with Gradle and Spring Boot.

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "vse_svoi_restfull"
---
+++ src/main/kotlin/com/vsesvoi/VsesvoiApplication.kt
@@ -0,0 +1,11 @@
+package com.vsesvoi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VsesvoiApplication
+
+fun main(args: Array<String>) {
+    runApplication<VsesvoiApplication>(*args)
+}
